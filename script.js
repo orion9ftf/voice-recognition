788c76811dba161377110cec6aeeb6413162eb5b
eorion <brpmdr@mailfence.com>
2022-04-28 22:28:55 -0400

    add event click

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const texto = document.getElementById('texto');
 //reconocimiento de voz según idioma:
 let recognition = new webkitSpeechRecognition();
 recognition.lang = 'es-ES';
+recognition.continuous = true;
+recognition.interimResults = false //espacios en blanco
+
+btnStartRecord.addEventListener('click', () => {
+  recognition.start();
+})
+
+btnStopRecord.addEventListener('click', () => {
+  recognition.abort();
+})
 
