2645bb8fd2f6646517b4c752310ec7dae9662546
eorion <brpmdr@mailfence.com>
2022-04-28 22:31:20 -0400

    recognition.onresult

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const btnStopRecord = document.getElementById('btnStopRecord');
 const texto = document.getElementById('texto');
 
 //reconocimiento de voz según idioma:
-let recognition = new webkitSpeechRecognition();
+const recognition = new webkitSpeechRecognition();
 recognition.lang = 'es-ES';
 recognition.continuous = true;
 recognition.interimResults = false //espacios en blanco
+
+recognition.onresult = (event) => {
+  const results = event.results;
+}
 
 btnStartRecord.addEventListener('click', () => {
   recognition.start();
