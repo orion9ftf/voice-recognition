459079fb962b857eba69d20dfa4d17c8d6f02d3d
eorion <brpmdr@mailfence.com>
2022-04-28 22:25:01 -0400

    voice and language

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const btnStartRecord = document.getElementById('btnStartRecord');
 const btnStopRecord = document.getElementById('btnStopRecord');
 const texto = document.getElementById('texto');
 
+//reconocimiento de voz según idioma:
+let recognition = new webkitSpeechRecognition();
+recognition.lang = 'es-ES';
+
